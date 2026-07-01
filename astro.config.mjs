9f8eda2a9f0902e166252abb874eda5acb80ce64
mrvauxs <mrvauxs@gmail.com>
2026-07-01 18:56:43 +0200

    Fix mdx tables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import remarkGfm from 'remark-gfm';
 
 // https://astro.build/config
 export default defineConfig({
+	markdown: {
+		remarkPlugins: [remarkGfm],
+	},
 	integrations: [
 		starlight({
 			title: 'Vauxs\' Wiki',
